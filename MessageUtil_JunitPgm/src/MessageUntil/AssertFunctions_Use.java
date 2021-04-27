4007569a5b3e4a34dc5cbe48f8ce6bc3b83d5f93
gbadavanache <64797242+gbadavanache@users.noreply.github.com>
2021-04-27 09:50:14 +0530

    Added some more assert mtds ,commit for AsserFunction_use

--- MessageUtil_JunitPgm/src/MessageUntil/AssertFunctions_Use.java
+++ MessageUtil_JunitPgm/src/MessageUntil/AssertFunctions_Use.java
@@ -2,6 +2,7 @@ package MessageUntil;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+
 public class AssertFunctions_Use {
 
 	@Test 
@@ -10,6 +11,17 @@ public class AssertFunctions_Use {
 		String temp = null;
 		String str = "Hands-on on Assert Functions";
 		
+		String str1 = "Heaven";
+		String str2 = null;
+		String str3 = "Earth";
+		String str4 = new String("Heaven");
+		
+		int a = 45;
+		int b = 30;
+		
+		String[] expectedArray = {"one","two","three"};
+		String[] resultArray = {"one","two","three"};
+		
 		//check for equality 
 		assertEquals("Hands-on on Assert Functions", str);
 		
@@ -17,8 +29,16 @@ public class AssertFunctions_Use {
 		assertFalse(num > 11);
 		
 		//check for NOT NULL Value
-		//assertNotNull(temp);
-
+		//assertNotNull(temp)
 		
+			
+			assertEquals(str1, str4);
+			assertTrue(a > b);
+			assertFalse(a < b);
+			assertNotNull(str1);
+			assertNull(str2);
+			assertNotSame(str3,str4);
+			assertArrayEquals(expectedArray,resultArray);
+		}
 	}
-}
+
